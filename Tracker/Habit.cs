54783ef2609569b65da152ed500d3903d449b676
mavukic <mvukic1@hotmail.com>
2024-10-29 19:25:25 +0100

    add page navigation button added, styling of addhabit and habittracker razor pages, added month navigation of trackers

--- Tracker/Habit.cs
+++ Tracker/Habit.cs
@@ -3,7 +3,7 @@
     public class Habit
     {
         public string Name { get; set; }
-        public Dictionary<int, bool> DailyCompletion { get; set; } = new();
-        public string Color { get; set; } = "#FFFFFF"; // Default color
+        public Dictionary<DateTime, bool> DailyCompletion { get; set; } = new(); // Updated to DateTime key
+        public string Color { get; set; } = "#FFFFFF";
     }
 }
